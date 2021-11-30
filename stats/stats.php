3cd930ccf781a3d9d5dae219132a5dfc5afdc43d
HeloNomy <yanismohamediestebe@gmail.com>
2021-11-30 17:41:23 +0100

    stats

--- stats/stats.php
+++ stats/stats.php
@@ -32,10 +32,58 @@
                     $temps-=60;
                     $heure+=1;
                 }
-                return $heure . " heure(s) et " . $temps . " minutes ";
+                if($heure>0){
+                    return $heure . " heures et " . $temps . " minutes ";    
+                }
+                return $temps . " minutes ";
             }
 
 
+    //âge des patients
+    $patient25=$conn->query("SELECT COUNT(id_patient) as nbPatient FROM `patient` WHERE (DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dateNaissance)), '%Y')+0) < 25 and civilite = 'Monsieur' ");
+    $patiente25=$conn->query("SELECT COUNT(id_patient) as nbPatient FROM `patient` WHERE (DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dateNaissance)), '%Y')+0) < 25 and civilite = 'Madame' ");
+    $patient2550=$conn->query("SELECT COUNT(id_patient) as nbPatient FROM `patient` WHERE (DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dateNaissance)), '%Y')+0) > 25 AND (DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dateNaissance)), '%Y')+0) < 50 and civilite = 'Monsieur' ");
+    $patiente2550=$conn->query("SELECT COUNT(id_patient) as nbPatient FROM `patient` WHERE (DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dateNaissance)), '%Y')+0) > 25 AND (DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dateNaissance)), '%Y')+0) < 50 and civilite = 'Madame' ");
+    $patient50=$conn->query("SELECT COUNT(id_patient) as nbPatient FROM `patient` WHERE (DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dateNaissance)), '%Y')+0) > 50 and civilite = 'Monsieur' ");
+    $patiente50=$conn->query("SELECT COUNT(id_patient) as nbPatient FROM `patient` WHERE (DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dateNaissance)), '%Y')+0) > 50 and civilite = 'Madame' ");
+
+    $h25 = $patient25->fetch();
+    $f25=$patiente25->fetch();
+    $h2550 = $patient2550->fetch();
+    $f2550=$patiente2550->fetch();
+    $h50 = $patient50->fetch();
+    $f50=$patiente50->fetch();
+    ?>      
+    
+    <table>
+  <tr>
+    <th>Tranche d'âge</th>
+    <th>Nb Hommes</th>
+    <th>Nb Femmes</th>
+  </tr>
+  <tr>
+    <td>Moins de 25 ans</td>
+    <td><?php echo $h25['nbPatient'] ?></td>
+    <td><?php echo $f25['nbPatient'] ?></td>
+  </tr>
+  <tr>
+    <td>Entre 25 et 50 ans</td>
+    <td><?php echo $h2550['nbPatient'] ?></td>
+    <td><?php echo $f2550['nbPatient'] ?></td>
+  </tr>
+  <tr>
+    <td>Plus de 50 ans</td>
+    <td><?php echo $h50['nbPatient'] ?></td>
+    <td><?php echo $f50['nbPatient'] ?></td>
+</tr>
+
+</table>
+
+
+    
+    <?php
+
+    //durée totale des consultations par médecin
     $idmed=$conn->query("SELECT id_Medecin FROM consulter GROUP BY id_Medecin");
     while ($exec = $idmed->fetch()){
         $dure=$conn->query("SELECT duree FROM consulter WHERE id_Medecin = $exec[id_Medecin]");
