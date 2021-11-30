e7bb81b74f2db912390755eae4210b15a85e2142
HeloNomy <yanismohamediestebe@gmail.com>
2021-11-30 16:04:32 +0100

    duree total

--- stats/stats.php
+++ stats/stats.php
@@ -15,13 +15,35 @@
     require "..\..\PhpGitCM\secu\secuConnexion.php";
     require "..\..\PhpGitCM\connect.php";
 
+    function temps_en_minutes($temps){
+                $time = explode(':', $temps);
+                $heure = $time[0];
+                $minutes = $time[1];
+                while($heure>0){
+                    $heure -= 1;
+                    $minutes += 60;
+                }
+                return $minutes;
+            }
+    
+    function nombre_heure($temps){
+                $heure=0;
+                while($temps>=60){
+                    $temps-=60;
+                    $heure+=1;
+                }
+                return $heure . " heure(s) et " . $temps . " minutes. ";
+            }
 
-    $idmed=$conn->query("SELECT id_Medecin FROM consulter");
+
+    $idmed=$conn->query("SELECT id_Medecin FROM consulter GROUP BY id_Medecin");
     while ($exec = $idmed->fetch()){
         $dure=$conn->query("SELECT duree FROM consulter WHERE id_Medecin = $exec[id_Medecin]");
+        $total = 0;
         while ($exec2 = $dure->fetch()){
-            echo $exec2['duree'];
+            $total += temps_en_minutes($exec2['duree']);
             }
+        echo 'le total est de ' . nombre_heure($total);
         }
 
 
