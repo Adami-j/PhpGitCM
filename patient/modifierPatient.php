abf74f71f2695626f4dd8c4c4491d4aa3b9de275
mkltr <mkltr309@gmail.com>
2021-12-30 21:26:41 +0100

    modif

--- patient/modifierPatient.php
+++ patient/modifierPatient.php
@@ -2,14 +2,15 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>modification patient</title>
+  <title>Modifier le patient</title>
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <h3 class="page-header text-center apie_m_cga">Modifier le patient</h3>
+    <hr>
 
 </head>
-<body>
-		
-		
+<form >
+
 
 		<?php  
 		
@@ -19,38 +20,18 @@
 		$id=$_GET['id'];
 		$req = $conn->query("SELECT * FROM patient WHERE id_patient= $id " );
 		$row=$req->fetch();
-		$req = $conn->query("SELECT nom, civilite, prenom, id_Medecin FROM medecin; " );
+
 		?>
-	<form action=""  method="POST" >
-		Civilité                   :</br><select name="selectCivilite"/> 
-										<?php if($row['civilite']=='Monsieur'){
-												echo "<option value='Monsieur'>Monsieur</option> 
-												<option value='Madame'>Madame</option></select></br>";
-										}else{
-											echo "<option value='Madame'>Madame</option> 
-												<option value='Monsieur'>Monsieur</option></select></br>";
-										}
-										?>
-											</select></br>
-		Nom                        :<input type="text" id="nom" name="nom" value="<?php echo $row['nom']; ?>" ></br>
-		Prénom                     :<input type="text" id="prenom" name="prenom"  value="<?php echo $row['prenom']; ?>"></br>
-		Adresse                    :<input type="text" id="adresse" name="adresse" value="<?php echo $row['adresse']; ?>"></br>
-		Ville                      :<input type="text" id="ville" name="ville"  value="<?php echo $row['ville']; ?>"></br>
-		Code postal                :<input type="number" id="cp" name="cp" min="01000" max="98800"  value="<?php echo $row['codePostal']; ?>" ></br>
-		Date de naissance          :<input type="date" id="dNaissance" name="dNaissance" min="1902-01-02" max="2021-11-25" value="<?php echo $row['dateNaissance']; ?>"></br>
-		Lieu naissance             :<input type="text" id="lNaissance" name="lNaissance" value="<?php echo $row['lieuNaissance']; ?>"></br>
-		Téléphone                  :<input type="number" id="tel" name="tel" value="<?php echo $row['telephone']; ?>" ></br>
-		Numéro de sécurité sociale :<input type="text" id="numSecu" name="numSecu" value="<?php echo $row['numeroSecu']; ?>"></br>
-		Médecin référent           :<select name="medecin"/> <option value="0">Pas de médecin référent</option>
-                <?php while($row=$req->fetch()){ ?>
-                        <option value="<?php echo $row['id_Medecin']?>"><?php echo $row['nom']?></option>
-                <?php }
-				$req->closeCursor();?>
-                </select></br>
-		<input type="submit" id="envoi" name="envoi" value='valider'>
-		<input type="submit" id="retour" name="retour" value='annuler'>
-		
-		
+
+
+
+
+
+
+
+
+</form>
+
 	<?php 
 			
 	if(isset($_POST['envoi'])){ 		
@@ -72,9 +53,7 @@
 					$requette= "UPDATE patient SET numeroSecu ='$numSecu',nom='$nom',prenom='$prenom',telephone='$telephone',adresse='$adresse',ville='$ville',codePostal='$codePostal',dateNaissance='$dNaissance',lieuNaissance='$lieuNaissance',civilite='$civilite' WHERE id_patient= $id";
 					$conn->exec($requette);
 					
-					$idm=$row['id_Medecin'];
-					$test =$conn->prepare("INSERT INTO référent(id_patient,O_N, Id_Medecin) VALUES (?,?,?);");
-					$test->execute('48', '1' , '11');
+
 					header('Location:patient.php');
 				
 				
@@ -83,11 +62,97 @@
 	if(isset($_POST['retour'])){
 		header('Location:patient.php');
 	}
-		
-			
-		
 		?>
 	</form>
+
+        <title>Modifier patient</title>
+        </head>
+
+        <body class="bg-light">
+        <div class="container">
+
+
+            <!-- Start Form -->
+            <div>
+                <h4>informations patient</h4>
+            </div>
+
+            <div class="needs-validation" novalidate>
+
+                <form action=""  method="POST" >
+
+                    <select name="selectCivilite"/>                     <?php if($row['civilite']=='Monsieur'){                         echo "<option value='Monsieur'>Monsieur</option>              <option value='Madame'>Madame</option></select></br>";                     }else{                         echo "<option value='Madame'>Madame</option>              <option value='Monsieur'>Monsieur</option></select></br>";                     }                     ?>
+
+
+
+                <div class="row">
+
+                    <div class="form-group col-md-6">
+
+                        <label for="nom">Nom</label>
+
+                        <input type="text" id="nom" name="nom" class="form-control "value="<?php echo $row['nom']; ?>" >
+
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label for="prenom">Prénom</label>
+                        <input type="text" id="prenom" name="prenom" class="form-control " value="<?php echo $row['prenom']; ?>">
+                    </div>
+
+
+
+
+                    <div class="col-md-12 mb-3">
+                        <label for="adresse">Adresse</label>
+                        <input type="text" id="adresse" name="adresse" class="form-control "value="<?php echo $row['adresse']; ?>">
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label for="ville">Ville</label>
+                        <input type="text" id="ville" name="ville" class="form-control" value="<?php echo $row['ville']; ?>">
+
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label for="cp">Code postal</label>
+                        <input type="number" id="cp" name="cp" class="form-control "min="01000" max="98800"  value="<?php echo $row['codePostal']; ?>" >
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="dNaissance">Date de naissance</label>
+                        <input type="date" id="dNaissance"class="form-control " name="dNaissance" min="1902-01-02" max="2021-11-25" value="<?php echo $row['dateNaissance']; ?>">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="lNaissance">Lieu de naissance</label>
+                        <input type="text" id="lNaissance" class="form-control "name="lNaissance" value="<?php echo $row['lieuNaissance']; ?>">
+
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="tel">Téléphone</label>
+                        <input type="number" id="tel" class="form-control "name="tel" value="<?php echo $row['telephone']; ?>" >
+
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="numSecu">Numéro de sécurité sociale</label>
+                        <input type="text" id="numSecu" class="form-control "name="numSecu" value="<?php echo $row['NumeroSecu']; ?>">
+
+                    </div>
+
+                    <div class="form-group">
+                    <input type="submit"  id="envoi" class="btn btn-outline-success" name="envoi">Submit</input>
+                    <input type="submit" id="retour" class="btn btn-outline-danger" name="retour">Reset</input>
+
+
+                    </div>
+                </div>
+                </div>
+            </form>
+            </div>
+
+        </div>
+
+
+
 </body>
 </html>
 
