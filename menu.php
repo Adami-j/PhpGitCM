e6a3eb400e341ed44203ebc1dfe6afabcb45eb6e
mkltr <mkltr309@gmail.com>
2022-01-01 16:02:17 +0100

    modif

--- menu.php
+++ menu.php
@@ -1,11 +1,12 @@
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 
 
 <nav class="navbar navbar-expand-md bg-dark">
     <a href="..\..\PhpGitCm\adminGestion.php" class="navbar-brand">Cabinet medical</a>
     <div class=" navbar-collapse">
         <ul class="navbar-nav m-auto">
-            <li class="nav-item active">
-                <a href="..\..\PhpGitCM\patient/patient.php" class="nav-link">Patient</a>
+            <li class="nav-item active"   >
+                <a href="..\..\PhpGitCM\patient/patient.php" class="nav-link" >Patient</a>
             </li>
             <li class="nav-item">
                 <a href="..\..\PhpGitCM\medecin/medecin.php" class="nav-link">Medecin</a>
